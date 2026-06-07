d5d2f16b5e170a17f33362795811646d728e43e4
Michael James Soria [mps0104] <mps0104@arastudent.ac.nz>
2026-06-07 19:07:39 +1200

    mobile size added2

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import OptionScene from "./scenes/OptionScene";
 import GameScene from "./scenes/GameScene";
 import Level2Scene from "./scenes/Level2Scene.js";
 import MarketScene from "./scenes/MarketScene.js";
+
+
+const isMobile = window.innerWidth < 768;
 const config = {
     type: Phaser.AUTO,
-    width: 2000,
-    height: 720,
+    // width: window.innerWidth,
+    // height: window.innerHeight,
+    width: isMobile ? 800 : 2000,
+    height: isMobile ? 1280 : 940,
     render: {
     antialias: false,
     pixelArt: false
