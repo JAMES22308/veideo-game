6cf9499651053f1f3a276bd1474a76db7d9cb49f
Michael James Soria [mps0104] <mps0104@arastudent.ac.nz>
2026-06-07 19:32:04 +1200

    mobile size added3

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ const config = {
     // height: window.innerHeight,
     width: isMobile ? 800 : 2000,
     height: isMobile ? 1280 : 940,
+
     render: {
     antialias: false,
     pixelArt: false
